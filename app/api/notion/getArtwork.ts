2c532fa58ba374934b67d179e57f93c20bd0a22c
Floriane <florianelefebvre.dev@gmail.com>
2024-08-23 18:40:03 +0200

    fixed piepline

--- app/api/notion/getArtwork.ts
+++ app/api/notion/getArtwork.ts
@@ -1,16 +1,16 @@
-import { NextResponse } from "next/server";
-import { Client } from "@notionhq/client";
+// import { NextResponse } from "next/server";
+// import { Client } from "@notionhq/client";
 
-const notion = new Client({ auth: process.env.NOTION_API_KEY });
+// const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
-export async function GetArtwork() {
-    const pageId = process.env;
+// export async function GetArtwork() {
+//     const pageId = process.env;
 
-    try {
-        const response = await notion.pages.retrieve({ page_id: pageId });
-        console.log({ response });
-        return NextResponse.json(response);
-    } catch (error) {
-        return NextResponse.json({ error: error.message }, { status: 500 });
-    }
-}
+//     try {
+//         const response = await notion.pages.retrieve({ page_id: pageId });
+//         console.log({ response });
+//         return NextResponse.json(response);
+//     } catch (error) {
+//         return NextResponse.json({ error: error.message }, { status: 500 });
+//     }
+// }
