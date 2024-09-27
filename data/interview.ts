49c99abaa6831d7cacd76ae0922587a750eec916
Floriane <florianelefebvre.dev@gmail.com>
2024-09-27 17:36:01 +0200

    debut correction texte

--- data/interview.ts
+++ data/interview.ts
@@ -1,32 +1,32 @@
 export const interview = [
     {
         question: "Comment décrirais-tu ton univers artistique ?",
-        answer: "C’est un mélange entre l’instinct brut et la réflexion profonde. Mon art est à la fois humble et audacieux, un pont entre le visible et l’invisible. C’est un univers où l’imagination n’a pas de limites, où chaque élément, chaque forme, a une histoire à raconter.",
+        answer: "C’est une question délicate.Je sais ce que j'aime et ce que j'ai plaisir à representer et à l'inverse ce qui me laisse perplexe,triste et remonté.Je dirais que mon univers est empreint d'organique,de fantastique et d'onirisme.Surréaliste dans sa conception,assez sombre dans ses thématiques avec une touche d'absurde et de poésie. Après je le répète c'est pas évident .",
     },
     {
         question: "Peux-tu nous parler de ton parcours ?",
-        answer: "Mon chemin a toujours été empreint de curiosité. J’ai fait des passages éclairs dans plusieurs écoles d’art, de communication visuelle, d’architecture d’intérieur, et aux Beaux-Arts. Mais finalement, je me suis dirigé vers la nature, vers le végétal. C'est là que je suis devenu fleuriste et paysagiste. Cependant, la nécessité de créer – de gribouiller, peindre, écrire – ne m’a jamais vraiment quitté.",
+        answer: "Mon chemin a toujours été peuplé d'expériences et de rencontres.J’ai fait des passages éclairs dans plusieurs écoles d’art,en communication visuelle/architecture d’intérieur, et aux Beaux-Arts. Mais finalement, je me suis dirigé vers la nature,le végétal. C'est là que je suis devenu fleuriste et paysagiste. Cependant, la nécessité de créer – de gribouiller, peindre, écrire – ne m’a jamais vraiment quitté.",
     },
     {
         question: "Qu’est-ce qui a déclenché ton retour vers l'art ?",
-        answer: "À un moment, la créativité est devenue une faim, quelque chose qui m'a littéralement dévoré. J’ai arrêté mon activité de paysagiste pour explorer de nouvelles façons de penser et de vivre. La permaculture et l’éco-construction sont devenues mes nouvelles passions. Puis est arrivée la Covid, un moment qui m'a rappelé à quel point la nature et l'art s'entrelacent. J’ai compris que je devais créer et m'enraciner quelque part.",
+        answer: "À un moment, j'en ai marre de mon taff et pousse en moi le germe de la création, cà me bouffe littéralement de l'intérieur.c'est vital. J'arrête mon activité de fleuriste pour explorer de nouvelles façons de penser et de vibrer le monde. La permaculture et l’éco-construction deviennent de nouvelles passions. Puis arrive la Covid, un moment qui me rappele à quel point la nature et l'art s'entrelacent. Je comprends que je dois créer et m'enraciner quelque part.",
     },
     {
-        question: "Pourquoi la Bretagne comme terre d’enracinement ?",
+        question: "Pourquoi la Bretagne comme ancrage?",
         answer: "La Bretagne, c’est le terreau parfait pour laisser libre cours à mon imagination. C'est un endroit où je peux me reconnecter à la terre et à mes créations. C’est ici que l’histoire de KINDA CREAPY commence. Un univers à la fois brut et sensible, où chaque forme a son langage, où la nature et l’art se répondent.",
     },
     {
         question:
             "Ton travail semble avoir une dimension mystique, notamment avec la question de la matrice. Peux-tu nous en dire plus ?",
-        answer: "J’ai toujours été fasciné par les formes cachées, les structures invisibles qui sous-tendent notre monde. Vivons-nous dans une matrice ? Qui sait. Mais pour moi, chaque création est une tentative de révéler ces formes, de donner vie à ce qui est souvent imperceptible. C’est ce qui me pousse à créer, à explorer des concepts au-delà de ce que l’on voit.",
+        answer: "Ahah,vaste question! J’ai toujours été fasciné par l'envers des choses,les sens cachés, j'interroge nos rapports au monde physique et tente d'aprehenderla réalité d' une maniére plus globale, en la requestionnant régulièrement. Vivons-nous dans une matrice ? Qui sait. Mais pour moi, chaque création est une tentative de révéler ces formes, de donner vie à ce qui est souvent imperceptible. C’est ce qui me pousse à créer, à explorer des concepts au-delà de ce que l’on voit.",
     },
     {
         question: "Quelles sont tes principales inspirations ?",
-        answer: "La nature, bien sûr, mais aussi l’archéologie et l’histoire des civilisations disparues. J’adore explorer les symboles, les mythes, les formes anciennes. L’écriture, elle aussi, est une grande source d’inspiration pour moi. Je suis toujours en quête de nouvelles façons de combiner le texte et l’image, de donner un sens profond à mes œuvres.",
+        answer: "Mes inspirations au sens d'amours et de passions sont variées,mon travail en regorge, j'en parle plus amplement dans mes thèmes et travaux.Globalement,j'aime la page blanche, c'est une amie que je noircis,le lettrage dans la recherche de graphismes.La nature au sens large,je suis nature ,m'en immerge et m'en inspire. L'archéologie,l'architecture antique,l'art rupestre et l'histoire des civilisations.Les théories scientifiques. Les mythologies et spiritualités.Le cabinet de curiosités dans la richesse du vivant et la collection d'objets;voila pour etre bref. Pour le reste c'est dur d'etre concis mais si je dois choisir, je dis: art brut, mouvement surréaliste et art fantastique,et quelques artistes dont j'aime particulierement le travail:Beksinski, Giger et Dali.Mais je me sens un peu mal de balancer cà comme cà, c'est un sujet passionnant et on pourrait en parler des journées entières...
     },
 
     {
         question: "Un dernier mot pour ceux qui découvrent ton travail ?",
-        answer: "Mon univers est fait pour être exploré, ressenti. J’invite chacun à plonger dans ces formes, à écouter ce que l’art et la nature ont à dire, et à laisser libre cours à sa propre imagination. Alors, accrochez-vous... et GAZ !",
+        answer: "Mon univers est fait pour être exploré, ressenti. J’invite chacun-e à plonger dans ces formes, à écouter et questionner ce que l’art et la nature ont à dire, et à laisser libre cours à sa propre imagination. Alors, accrochez-vous... et GAZ !",
     },
 ];
