ad4e7092d63f9a2bac3d3a3782cc26be1838fefc
Floriane <florianelefebvre.dev@gmail.com>
2024-10-09 21:34:31 +0200

    new images

--- imageImports.js
+++ imageImports.js
@@ -1,43 +1,59 @@
-import img1 from "./public/images/artpieces/IMG_3003.webp";
-import img2 from "./public/images/artpieces/IMG_3237.webp";
-import img3 from "./public/images/artpieces/IMG_3846.webp";
-import img4 from "./public/images/artpieces/IMG_4784.webp";
-import img5 from "./public/images/artpieces/IMG_4802.webp";
-import img6 from "./public/images/artpieces/IMG_4811.webp";
-import img7 from "./public/images/artpieces/IMG_4919.webp";
-import img8 from "./public/images/artpieces/IMG_4930.webp";
-import img9 from "./public/images/artpieces/IMG_4939.webp";
-import img10 from "./public/images/artpieces/IMG_4942.webp";
-import img11 from "./public/images/artpieces/IMG_4950.webp";
-import img12 from "./public/images/artpieces/IMG_5506.webp";
-import img13 from "./public/images/artpieces/IMG_5509.webp";
-import img14 from "./public/images/artpieces/IMG_5511.webp";
-import img15 from "./public/images/artpieces/IMG_5809.webp";
-import img16 from "./public/images/artpieces/IMG_5812.webp";
-import img17 from "./public/images/artpieces/IMG_5814.webp";
-import img18 from "./public/images/artpieces/IMG_5815.webp";
-import img19 from "./public/images/artpieces/orobouros.webp";
+import img1 from "./public/images/artpieces/AB024B01.webp";
+import img2 from "./public/images/artpieces/AC024PK02.webp";
+import img3 from "./public/images/artpieces/AG024B02.webp";
+import img4 from "./public/images/artpieces/B001B01.webp";
+import img5 from "./public/images/artpieces/BL024B01.webp";
+import img6 from "./public/images/artpieces/BL024B02.webp";
+import img7 from "./public/images/artpieces/BL024B03.webp";
+import img8 from "./public/images/artpieces/BL024B04.webp";
+import img9 from "./public/images/artpieces/IMG_3003.webp";
+import img10 from "./public/images/artpieces/IMG_3237.webp";
+import img11 from "./public/images/artpieces/IMG_3846.webp";
+import img12 from "./public/images/artpieces/IMG_4784.webp";
+import img13 from "./public/images/artpieces/IMG_4802.webp";
+import img14 from "./public/images/artpieces/IMG_4811.webp";
+import img15 from "./public/images/artpieces/IMG_4919.webp";
+import img16 from "./public/images/artpieces/IMG_4930.webp";
+import img17 from "./public/images/artpieces/IMG_4939.webp";
+import img18 from "./public/images/artpieces/IMG_4942.webp";
+import img19 from "./public/images/artpieces/IMG_4950.webp";
+import img20 from "./public/images/artpieces/IMG_5506.webp";
+import img21 from "./public/images/artpieces/IMG_5509.webp";
+import img22 from "./public/images/artpieces/IMG_5511.webp";
+import img23 from "./public/images/artpieces/IMG_5809.webp";
+import img24 from "./public/images/artpieces/IMG_5812.webp";
+import img25 from "./public/images/artpieces/IMG_5814.webp";
+import img26 from "./public/images/artpieces/IMG_5815.webp";
+import img27 from "./public/images/artpieces/orobouros.webp";
 
 const imageMap = {
-    "IMG_3003.webp": img1,
-    "IMG_3237.webp": img2,
-    "IMG_3846.webp": img3,
-    "IMG_4784.webp": img4,
-    "IMG_4802.webp": img5,
-    "IMG_4811.webp": img6,
-    "IMG_4919.webp": img7,
-    "IMG_4930.webp": img8,
-    "IMG_4939.webp": img9,
-    "IMG_4942.webp": img10,
-    "IMG_4950.webp": img11,
-    "IMG_5506.webp": img12,
-    "IMG_5509.webp": img13,
-    "IMG_5511.webp": img14,
-    "IMG_5809.webp": img15,
-    "IMG_5812.webp": img16,
-    "IMG_5814.webp": img17,
-    "IMG_5815.webp": img18,
-    "orobouros.webp": img19,
+    "AB024B01.webp": img1,
+    "AC024PK02.webp": img2,
+    "AG024B02.webp": img3,
+    "B001B01.webp": img4,
+    "BL024B01.webp": img5,
+    "BL024B02.webp": img6,
+    "BL024B03.webp": img7,
+    "BL024B04.webp": img8,
+    "IMG_3003.webp": img9,
+    "IMG_3237.webp": img10,
+    "IMG_3846.webp": img11,
+    "IMG_4784.webp": img12,
+    "IMG_4802.webp": img13,
+    "IMG_4811.webp": img14,
+    "IMG_4919.webp": img15,
+    "IMG_4930.webp": img16,
+    "IMG_4939.webp": img17,
+    "IMG_4942.webp": img18,
+    "IMG_4950.webp": img19,
+    "IMG_5506.webp": img20,
+    "IMG_5509.webp": img21,
+    "IMG_5511.webp": img22,
+    "IMG_5809.webp": img23,
+    "IMG_5812.webp": img24,
+    "IMG_5814.webp": img25,
+    "IMG_5815.webp": img26,
+    "orobouros.webp": img27,
 };
 
 export default imageMap;
