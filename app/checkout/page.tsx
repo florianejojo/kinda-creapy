c8441d1bb6ea04c66440cdbe035d6e8cffc393ca
Floriane Lefebvre <floriane.testwe@gmail.com>
2025-06-06 12:13:13 +0100

    checkout page

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -7,8 +7,13 @@ export default function CheckoutPage() {
   const searchParams = useSearchParams()
   const productId = searchParams.get("productId")
   return (
-    <div className="min-h-screen flex items-center justify-center p-6">
-      <Checkout productId={productId} />
+    <div className="min-h-screen bg-stone-50 flex items-center justify-center p-6">
+      <div className="w-full max-w-md bg-white shadow-xl rounded-xl p-6">
+        <h2 className="text-2xl font-bold mb-6 text-center text-black">
+          Paiement
+        </h2>
+        <Checkout priceId={"priceId"} />
+      </div>
     </div>
   )
 }
