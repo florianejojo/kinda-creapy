8731041b3e56c49ebde238a1d4aaf39cf82987f7
waskalien <lefebv.antoine@gmail.com>
2025-11-07 09:55:54 +0100

    fix(admin): adjust delete confirmation text and opacity

--- components/delete_confirmation.tsx
+++ components/delete_confirmation.tsx
@@ -17,7 +17,7 @@ const DeleteConfirmation: React.FC<DeleteConfirmationProps> = ({
 
   return (
     <div
-      className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
+      className="fixed inset-0 flex items-center justify-center bg-black/90 z-50"
       onClick={onCancel}
     >
       <div
@@ -25,10 +25,10 @@ const DeleteConfirmation: React.FC<DeleteConfirmationProps> = ({
         onClick={(e) => e.stopPropagation()}
       >
         <h2 className="text-lg font-bold mb-4 text-gray-800 dark:text-gray-100">
-          Confirm Deletion
+          Confirmer la suppression
         </h2>
         <p className="text-gray-600 dark:text-gray-300">
-          Are you sure you want to delete the product{" "}
+          Es-tu sûr de vouloir supprimer le produit{" "}
           <strong className="text-gray-800 dark:text-gray-200">{title} </strong>?
         </p>
         <div className="flex justify-end mt-6">
@@ -36,13 +36,13 @@ const DeleteConfirmation: React.FC<DeleteConfirmationProps> = ({
             className="bg-gray-300 hover:bg-gray-400 text-gray-700 font-semibold py-2 px-4 rounded mr-2 dark:bg-gray-700 dark:hover:bg-gray-600 dark:text-gray-200"
             onClick={onCancel}
           >
-            Cancel
+            Annuler
           </button>
           <button
             className="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded dark:bg-red-600 dark:hover:bg-red-700"
             onClick={onConfirm}
           >
-            Delete
+            Supprimer
           </button>
         </div>
       </div>
